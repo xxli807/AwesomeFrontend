de10f86290d5af4bfc9d47eb2e57e1e3f4571d5f
unknown <lxx871030@gmail.com>
2021-04-18 21:40:51 +1000

    check the rerender

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -3,8 +3,13 @@ import { render, screen } from '@testing-library/react';
 import App from '../App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  const { rerender } = render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+
+  rerender(<App />);
+  const linkElement1 = screen.getByText(/learn react/i);
+  expect(linkElement1).toBeInTheDocument();
+ 
 }); 
 
